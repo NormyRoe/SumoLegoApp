3c72ae7dcfa417be78cdd2ac999aba8a5fb59f65
Iryna Suprunovych <iryna.suprunovych@gmail.com>
2023-11-21 00:02:23 +1000

    Draw app

--- drawapp/tests.py
+++ drawapp/tests.py
@@ -1,74 +1,205 @@
+from datetime import timedelta
+from urllib import request
 from django.test import TestCase
 from django.utils import timezone
 from drawapp.algorithm import GenerateAllRounds, ValidateRounds
+from drawapp.utils import GenerateRoundsForCompetition
+from drawapp.views import CreateDrawForCompetition
 import legosumodb.models
+from django.db.models import Count
 
 # Create your tests here.
 
 
+def GenerateDummyData(self):
+    self.divisionNames = ["Math", "Engineering", "Technology", "Butterflies"]
+    self.competitionNames = ["Spring Game", "Summer Game"]
+    
+    self.school = {}
+    self.team = {}
+    for schoolNumber in range(1, 5):
+        self.school[schoolNumber] = legosumodb.models.School()
+        self.school[schoolNumber].name = f"School {schoolNumber}"
+        self.school[schoolNumber].street_address_line_1 = ""
+        self.school[schoolNumber].street_address_line_2 = ""
+        self.school[schoolNumber].suburb = ""
+        self.school[schoolNumber].state = ""
+        self.school[schoolNumber].postcode = ""
+        self.school[schoolNumber].contact_name = ""
+        self.school[schoolNumber].contact_number = ""
+        self.school[schoolNumber].email_address = ""
+        self.school[schoolNumber].paid = True
+        self.school[schoolNumber].save()
+        
+        for divisionName in self.divisionNames:
+            teamName = f"Team {divisionName} from School {schoolNumber}"
+            self.team[teamName] = legosumodb.models.Team()
+            self.team[teamName].name = teamName
+            self.team[teamName].school = self.school[schoolNumber]
+            self.team[teamName].save()
+    
+    # Create divisions
+    self.division = {}
+    for divisionName in self.divisionNames:
+        self.division[divisionName] = legosumodb.models.Division()
+        self.division[divisionName].name = divisionName
+        self.division[divisionName].save()
+        
+    self.competition = {}
+    for competitionName in self.competitionNames:
+        self.competition[competitionName] = legosumodb.models.Competition()
+        self.competition[competitionName].name = competitionName
+        self.competition[competitionName].games_per_team = 4
+        self.competition[competitionName].date = timezone.now()
+        self.competition[competitionName].save()
+        
+        # Associate division with competition
+        self.divisionhascompetition = {}
+        for divisionName in self.divisionNames:
+            self.divisionhascompetition[divisionName] = {}
+            self.divisionhascompetition[divisionName][competitionName] = legosumodb.models.DivisionHasCompetition()
+            self.divisionhascompetition[divisionName][competitionName].competition = self.competition[competitionName]
+            self.divisionhascompetition[divisionName][competitionName].division = self.division[divisionName]
+            self.divisionhascompetition[divisionName][competitionName].nbr_of_fields = 2
+            self.divisionhascompetition[divisionName][competitionName].save()
+        
+        # Add teams for each school into each division
+        oddTeamAdded = False
+        for schoolNumber in self.school:
+            for divisionName in self.divisionNames:
+                teamName = f"Team {divisionName} from School {schoolNumber}"
+                division_team = legosumodb.models.DivisionHasTeam()
+                division_team.division = self.division[divisionName]
+                division_team.team = self.team[teamName]
+                division_team.save()
+
+                checked_in = legosumodb.models.CheckedIn()
+                checked_in.competition_id = self.competition[competitionName]
+                checked_in.division_id = self.division[divisionName]
+                checked_in.team_id = self.team[teamName]
+                checked_in.checked_in = True
+                checked_in.save() 
+                
+            if not oddTeamAdded:
+                oddTeamAdded = True
+                teamName = f"Team Odd {divisionName} from School {schoolNumber}"
+                
+                self.team[teamName] = legosumodb.models.Team()
+                self.team[teamName].name = teamName
+                self.team[teamName].school = self.school[schoolNumber]
+                self.team[teamName].save()
+                
+                division_team = legosumodb.models.DivisionHasTeam()
+                division_team.division = self.division[divisionName]
+                division_team.team = self.team[teamName]
+                division_team.save()
+
+                checked_in = legosumodb.models.CheckedIn()
+                checked_in.competition_id = self.competition[competitionName]
+                checked_in.division_id = self.division[divisionName]
+                checked_in.team_id = self.team[teamName]
+                checked_in.checked_in = True
+                checked_in.save() 
+                
+    # Create fields
+    self.fields = {}
+    self.divisionFields = {}
+    for divisionName in self.divisionNames:
+        self.fields[divisionName] = []
+        self.divisionFields[divisionName] = []
+        
+        numberOfFields = list(self.divisionhascompetition[divisionName].values())[0].nbr_of_fields
+        for fieldIndex in range(0, numberOfFields):
+            # Create field
+            field = legosumodb.models.Field()
+            field.name = f"Field {fieldIndex+1} for {divisionName}"
+            field.save()
+            
+            # Associate division with field
+            divisionField = legosumodb.models.DivisionHasField()
+            divisionField.field = field
+            divisionField.division = self.division[divisionName]
+            divisionField.save()
+            
+            self.fields[divisionName].append(field)
+            self.divisionFields[divisionName].append(divisionField)
+
+
+
 class Test(TestCase):
     def setUp(self):
-        # data required to make a draw
-        self.competition = legosumodb.models.Competition()
-        self.competition.name = "game"
-        self.competition.games_per_team = 4
-        self.competition.date = timezone.now()
-        self.competition.save()
-        
-        self.division = legosumodb.models.Division()
-        self.division.name = "division"
-        self.division.save()
-        
-        self.divisionhascompetition1 = legosumodb.models.DivisionHasCompetition()
-        self.divisionhascompetition1.competition = self.competition
-        self.divisionhascompetition1.division = self.division
-        self.divisionhascompetition1.nbr_of_fields = 4
-        self.divisionhascompetition1.save()
-        
-        self.school = legosumodb.models.School()
-        self.school.name = "School 1"
-        self.school.street_address_line_1 = ""
-        self.school.street_address_line_2 = ""
-        self.school.suburb = ""
-        self.school.state = ""
-        self.school.postcode = ""
-        self.school.contact_name = ""
-        self.school.contact_number = ""
-        self.school.email_address = ""
-        self.school.paid = True
-        self.school.save()
-        
-        for team_number in range(1, 9):
-            self.team = legosumodb.models.Team()
-            self.team.name = "Team "+str(team_number)
-            self.team.school = self.school
-            self.team.save()
-
-            self.checked_in = legosumodb.models.CheckedIn()
-            self.checked_in.competition_id = self.competition
-            self.checked_in.division_id = self.division
-            self.checked_in.team_id = self.team
-            self.checked_in.checked_in = True
-            self.checked_in.save() 
+        GenerateDummyData(self)
                 
     def test_can_retrieve_data(self):
-        """
-        check that we can get data from the database
-        """
         checkins = legosumodb.models.CheckedIn.objects.all()
-        self.assertEqual(len(checkins), 8)
         for checkin in checkins:
             print("Found checkin for team `", checkin.team_id.name, "` into competition `", checkin.competition_id.name, "`", sep="")
-    
-    
-    def test_create_draw(self):
-        team_1 = legosumodb.models.Team.objects.get(name = "Team 1")
-        self.assertEqual(team_1.school.name, "School 1")
-    
+        self.assertEqual(len(checkins), len(self.team)*len(self.competitionNames))
+        
 
+    def test_can_get_teams_checked_into_the_current_competition(self):
+        teamsInCurrentCompetition = legosumodb.models.Competition.objects.filter(
+            name__contains=self.competitionNames[0],
+            checkedin__checked_in=True
+        )
+        
+        # There should be 16 teams
+        self.assertEqual(len(teamsInCurrentCompetition), len(self.team))
+        
+    def test_can_get_count_of_teams_for_each_division_in_current_competition(self):
+        startTime = timezone.datetime.now() + timedelta(minutes=5)
+        GenerateRoundsForCompetition(self.competition[self.competitionNames[0]], startTime)
+        gameResults = list(legosumodb.models.GameResult.objects.all())
+        self.assertTrue(len(gameResults) > 0)
+        
+        for result in gameResults:
+            division = result.division
+            team_1 = result.team1
+            team_2 = result.team2
+            gameNumber = result.round
+            field = result.field
+            gameStartTime = result.start_time
+            competition = result.competition
+            
+            if team_2 != None:
+                print(
+                    f"Division {division.name}, Competition {competition.name}: Found game for round {gameNumber+1}" + 
+                    f" between \"{team_1.name}\" (score: {result.team1_points}) and \"{team_2.name}\" (score: {result.team2_points}) on" +
+                    f" field \"{field.name}\" with scheduled at {gameStartTime}"
+                )
+            else:
+                print(
+                    f"Division {division.name}, Competition {competition.name}: Found bye for round {gameNumber+1}" + 
+                    f" for \"{team_1.name}\" (score: {result.team1_points}) scheduled at {gameStartTime}"
+                )
+            
+    
+    
+class TestView(TestCase):
+    def test_can_call_api_and_get_404_for_non_existant_competition(self):
+        apirequest = request.Request("http://localhost", method="POST")
+        result = CreateDrawForCompetition(apirequest, 123456789)
+        self.assertEqual(result.status_code, 404)
+    
+    def test_can_call_api_and_get_200_for_known_competition(self):
+        GenerateDummyData(self)
+        competition = legosumodb.models.Competition.objects.get(
+            name=self.competitionNames[0]
+        )
+        
+        apirequest = request.Request("http://localhost", method="POST")
+        result = CreateDrawForCompetition(apirequest, competition.competition_id)
+        
+        self.assertEqual(result.status_code, 200)
+        gameResults = list(legosumodb.models.GameResult.objects.all())
+        self.assertTrue(len(gameResults) > 0)
+    
+    
+    
+    
+    
 class TestPairingAlgorithm(TestCase):
     def test_can_pair_two_teams(self):
-        #
         pairings_all_rounds = GenerateAllRounds(number_of_teams = 2)
         self.assertEqual(len(pairings_all_rounds), 1)
         self.assertTrue(ValidateRounds(pairings_all_rounds))
@@ -83,13 +214,18 @@ class TestPairingAlgorithm(TestCase):
         self.assertEqual(len(pairings_all_rounds), 3)
         self.assertTrue(ValidateRounds(pairings_all_rounds))
         
-    def test_can_pair_twelve_teams(self):
-        pairings_all_rounds = GenerateAllRounds(number_of_teams = 12)
-        self.assertEqual(len(pairings_all_rounds), 11)
+    def test_can_pair_five_teams(self):
+        pairings_all_rounds = GenerateAllRounds(number_of_teams = 5)
+        self.assertEqual(len(pairings_all_rounds), 5)
         self.assertTrue(ValidateRounds(pairings_all_rounds))
         
     def test_can_pair_seventeen_teams(self):
         pairings_all_rounds = GenerateAllRounds(number_of_teams = 17)
         self.assertEqual(len(pairings_all_rounds), 17)
+        self.assertTrue(ValidateRounds(pairings_all_rounds))
+        
+    def test_can_pair_eleven_team(self):
+        pairings_all_rounds = GenerateAllRounds(number_of_teams =  11)
+        self.assertEqual(len(pairings_all_rounds), 11)
         self.assertTrue(ValidateRounds(pairings_all_rounds))
         
--- drawapp/urls.py
+++ drawapp/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    # Create or clear and recreate a draw for the given competition.
-    path('<str:competition_id>/create', views.CreateDrawForCompetition, name='CreateDrawForCompetition'),
-    
-    # Get the draw for the given competition. If draw does not exist, it will be automatically created.
-    path('<str:competition_id>', views.GetDrawForCompetition, name='GetDrawForCompetition'),
+    # Create draw for the given competition.
+    path('<int:competition_id>', views.CreateDrawForCompetition, name='CreateDrawForCompetition'),
 ]
